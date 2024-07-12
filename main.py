4824b4d43f1a72c21f70babebb2a3d104994a8c0
Disitha <disithar@gmail.com>
2024-07-12 11:55:59 +0530

    get position of hands

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 
 # Initialize the webcam to capture video
-# The '2' indicates the third camera connected to your computer; '0' would usually refer to the built-in camera
+# The '0' indicates the first camera connected to your computer; adjust if needed
 cap = cv2.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,680)
+cap.set(3, 1280)
+cap.set(4, 680)
 
 # Initialize the HandDetector class with the given parameters
-detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+detector = HandDetector(staticMode=False, maxHands=1, modelComplexity=1, detectionCon=0.7, minTrackCon=0.5)
 
 def getHandInfo(img):
     # Find hands in the current frame
@@ -24,25 +24,36 @@ def getHandInfo(img):
         lmList = hand["lmList"]  # List of 21 landmarks for the first hand
         # Count the number of fingers up for the first hand
         fingers = detector.fingersUp(hand)
-        print(fingers)
-        return fingers,lmList
+        return fingers, lmList
     else:
         return None
 
+def draw(img, info, prev_pos):
+    fingers, lmList = info
+    current_pos = None
+    if fingers == [0, 1, 0, 0, 0]:
+        current_pos = lmList[8][0:2]
+        if prev_pos is None:
+            prev_pos = current_pos
+        cv2.line(img, tuple(current_pos), tuple(prev_pos), (255, 0, 255), 10)
+    return current_pos
 
+prev_pos = None
 # Continuously get frames from the webcam
 while True:
     # Capture each frame from the webcam
     # 'success' will be True if the frame is successfully captured, 'img' will contain the frame
     success, img = cap.read()
+    if not success:
+        break
+
     info = getHandInfo(img)
     if info:
-        fingers,lmList = info
-        print(fingers)
+        fingers, lmList = info
+        prev_pos = draw(img, info, prev_pos)
 
     # Display the image in a window
     cv2.imshow("Image", img)
 
     # Keep the window open and update it for each frame; wait for 1 millisecond between frames
     cv2.waitKey(1)
-
